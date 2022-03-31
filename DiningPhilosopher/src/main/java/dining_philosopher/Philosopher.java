8db792705d82bc262e7d43863695d1b76f244a39
PMolnes <pettermolnes@icloud.com>
2022-03-31 09:41:44 +0200

    Created a method in DiningPhilosopher and made Philosopher runnable.
    Created a test method in DiningPhilosopher which lets a given Philosopher
    eat if his neighbours are not eating.

--- DiningPhilosopher/src/main/java/dining_philosopher/Philosopher.java
+++ DiningPhilosopher/src/main/java/dining_philosopher/Philosopher.java
@@ -1,6 +1,6 @@
 package dining_philosopher;
 
-public class Philosopher {
+public class Philosopher implements Runnable {
 
     private Chopstick leftChopstick;
     private Chopstick rightChopstick;
@@ -8,5 +8,10 @@ public class Philosopher {
     public Philosopher(Chopstick leftChopstick, Chopstick rightChopstick) {
         this.leftChopstick = leftChopstick;
         this.rightChopstick = rightChopstick;
+    }
+
+    @Override
+    public void run() {
+        
     }
 }
