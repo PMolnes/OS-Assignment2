ad1ac91936603ee8b4526673ee390fcf265b07a9
PMolnes <pettermolnes@icloud.com>
2022-04-03 18:54:06 +0200

    Deleted the Chopstick class as it wasn't needed. Also wrote some documentation.

--- DiningPhilosopher/src/main/java/dining_philosopher/Monitor.java
+++ DiningPhilosopher/src/main/java/dining_philosopher/Monitor.java
@@ -3,12 +3,23 @@ package dining_philosopher;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * This is a Monitor class which keeps track of the shared resources
+ * (chopsticks). It checks wether neighbouring Philosophers are eating,
+ * before given Philosopher decides to eat.
+ */
 public class Monitor {
     private final ReentrantLock lock;
     private final Condition[] self;
     private State state[];
 
     public Monitor() {
+        /**
+         * This creates a new lock where the "Fairness" parameter is set to true.
+         * This lets the thread that has waited the longest get access to it first,
+         * if multiple threads try to access it at the same time. Overall, this
+         * fixes the starvation problem.
+         */
         lock = new ReentrantLock(true);
         self = new Condition[5];
         state = new State[5];
@@ -28,6 +39,11 @@ public class Monitor {
         }
     }
 
+    /**
+     * Tests if the chopsticks next to the given Philosopher
+     * are available.
+     * @param i Philosopher that tries to pickup chopsticks.
+     */
     private void availableChopsticks(int i) {
         if ((state[(i + 1) % 5] != State.EATING) && (state[(i + 4) % 5] != State.EATING) && (state[i] == State.HUNGRY)) {
             state[i] = State.EATING;
@@ -36,6 +52,13 @@ public class Monitor {
         }
     }
 
+    /**
+     * Entered Philosopher gets hungry and tries to pick acquire the shared
+     * resources (chopsticks). If the chopsticks are available, he will start
+     * eating, or he will wait until the chopsticks are available.
+     * @param i the Philosopher that wants to eat.
+     * @throws InterruptedException
+     */
     private void pickup(int i) throws InterruptedException {
         lock.lock();
         state[i] = State.HUNGRY;
--- DiningPhilosopher/src/main/java/dining_philosopher/Philosopher.java
+++ DiningPhilosopher/src/main/java/dining_philosopher/Philosopher.java
@@ -10,10 +10,15 @@ public class Philosopher extends Thread {
         this.monitor = monitor;
     }
 
+    /**
+     * Each thread (Philosopher) tries to eat 10 times.
+     * This can easily be turned into an infinite loop with a
+     * while (true) loop.
+     */
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
-            monitor.tryEat(id);
+            monitor.tryEat(this.id);
         }
     }
 }
